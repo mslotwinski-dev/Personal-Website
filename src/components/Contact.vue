0ca3a4fec67835a0cb4e1e20cd222b9144f6197d
Mateusz <mateuszslotwinski16@gmail.com>
2021-07-26 22:38:40 +0200

    Done!

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,21 +1,32 @@
 <template>
   <article>
     <h1>Contact</h1>
-      <form action="https://formspree.io/f/xgernwlz" method="POST">
-          <input type="text" id="Name" name="Name" placeholder="First and last name" />
-          <input type="text" id="Email" name="Email" placeholder="E-mail" />
-          <input type="text" id="Subject" name="Subject" placeholder="Subject" />
-          <textarea id="Content" name="Content" placeholder="Content of message" />
-          <div><button type="submit">Submit</button></div>
+      <form class='contact-form' @submit.prevent="sendEmail">
+          <input type="text" id="Name" name="user_name" placeholder="First and last name" />
+          <input type="email" id="Email" name="user_email" placeholder="E-mail" />
+          <input type="text" id="Subject" name="subject" placeholder="Subject" />
+          <textarea id="Content" name="message" placeholder="Content of message" />
+          <div><input type="submit" value="Send"/></div>
       </form>
   </article>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import emailjs from 'emailjs-com'
 
 export default defineComponent({
-  name: 'Start'
+  methods: {
+    sendEmail: (e:any) => {
+      emailjs.sendForm('service_fsc7z1s', 'template_o4685da', e.target, 'user_p0k5jW3dTiblvpU5h4UvO')
+        .then((result) => {
+          alert('SUCCESS!')
+        }, (error) => {
+          alert('FAILED...' + error)
+        })
+    }
+  },
+  name: 'Contact'
 })
 </script>
 
@@ -35,7 +46,7 @@ form {
   width:900px;
 }
 
-input[type="text"], textarea, select, button {
+input[type="text"], input[type="email"], textarea, select, input[type="submit"] {
   font-size:15px;
   padding:5px 8px;
   margin:5px;
